f5dfeae890de273f66452d9ba28f125e2f8e5f57
Raymond Gregory <vinedsel@gmail.com>
2020-12-27 20:54:11 -0600

    Refactored the Todos code using the setState hook

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -1,36 +1,63 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class InputTodo extends Component {
-  state = {
-    title: ""
-  };
-  onChange = e => {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+const InputTodo = props => {
+  const [title, setTitle] = useState("");
+
+  const onChange = e => {
+    setTitle(e.target.value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    this.props.addTodoProps(this.state.title);
-    this.setState({
-      title: ""
-    });
+    props.addTodoprops(title);
+    setTitle("");
   };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className="form-container">
-        <input
-          type="text"
-          className="input-text"
-          placeholder="Add todo..."
-          value={this.state.title}
-          name="title"
-          onChange={this.onChange}
-        />
-        <input type="submit" className="input-submit" value="Submit" />
-      </form>
-    );
-  }
-}
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add Todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+    </form>
+  );
+};
+
+// class InputTodo extends Component {
+//   state = {
+//     title: ""
+//   };
+//   onChange = e => {
+//     this.setState({
+//       [e.target.name]: e.target.value
+//     });
+//   };
+
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     this.props.addTodoProps(this.state.title);
+//     this.setState({
+//       title: ""
+//     });
+//   };
+//   render() {
+//     return (
+//       <form onSubmit={this.handleSubmit} className="form-container">
+//         <input
+//           type="text"
+//           className="input-text"
+//           placeholder="Add todo..."
+//           value={this.state.title}
+//           name="title"
+//           onChange={this.onChange}
+//         />
+//         <input type="submit" className="input-submit" value="Submit" />
+//       </form>
+//     );
+//   }
+// }
 export default InputTodo;
