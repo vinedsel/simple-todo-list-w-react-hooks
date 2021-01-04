3d83eba1777a8a426203f3b7734bd30885e39f57
Raymond Gregory <vinedsel@gmail.com>
2021-01-03 23:21:18 -0600

    Refactored code and introduced the useEffect hook and controller array on the header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,32 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 
-class Header extends Component {
-  componentDidUpdate(prevProps, prevState) {
+const Header = props => {
+  
+  const headerStyle = {
+    padding: "20px 0",
+    lineHeight: "2em"
+  };
+
+  useEffect(() => {
     var x = Math.floor(Math.random() * 256);
     var y = Math.floor(Math.random() * 256);
     var z = Math.floor(Math.random() * 256);
     var bgColor = "rgb(" + x + "," + y + "," + z + ")";
 
-    if (prevProps.headerSpan !== this.props.headerSpan) {
-      document.getElementById("inH1").innerHTML = "clicked";
-      document.getElementById("inH1").style.backgroundColor = bgColor;
-    }
-  }
+    document.getElementById("inH1").innerHTML = "clicked";
+    document.getElementById("inH1").style.backgroundColor = bgColor;
+  }, [props.headerSpan]);
 
-  render() {
-    const headerStyle = {
-      padding: "20px 0",
-      lineHeight: "2em"
-    };
-    return (
-      <header style={headerStyle}>
-        <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
-          Simple Todo App <span id="inH1"></span>
-        </h1>
-        <p style={{ fontSize: "19px" }}>
-          Please add to-dos item(s) through the input field
-        </p>
-      </header>
-    );
-  }
-}
+  return (
+    <header style={headerStyle}>
+      <h1 style={{ fontSize: "25px", marginBottom: "15px" }}>
+        Simple Todo App <span id="inH1"></span>
+      </h1>
+      <p style={{ fontSize: "19px" }}>
+        Please add to-dos item(s) through the input field
+      </p>
+    </header>
+  );
+};
 
 export default Header;
