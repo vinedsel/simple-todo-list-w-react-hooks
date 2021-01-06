b90fa2d532d5cedeafc5cb45f2b23109830c9904
Raymond Gregory <vinedsel@gmail.com>
2021-01-05 18:06:09 -0600

    Changed class-based to function-based logic

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,21 +1,19 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-class TodosList extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            handleChangeProps={this.props.handleChangeProps}
-            deleteTodoProps={this.props.deleteTodoProps}
-          />
-        ))}
-      </div>
-    );
-  }
-}
+const TodosList = props => {
+  return (
+    <div>
+      {props.todos.map(todo => (
+        <TodoItem
+          key={todo.id}
+          todo={todo}
+          handleChangeProps={props.handleChangeProps}
+          deleteTodoProps={props.deleteTodoProps}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default TodosList;
